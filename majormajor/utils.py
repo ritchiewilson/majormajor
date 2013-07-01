bb289271ee813b8ba66d45adaf0aca7f8c86bce7
ritchiew <ritchie.a.f.wilson@gmail.com>
2013-07-01 11:18:30 -0400

    helpful wrapper to count function calls

--- majormajor/utils.py
+++ majormajor/utils.py
@@ -34,3 +34,12 @@ def build_changeset_from_dict(m, doc=None):
         op = Op(j['action'],j['path'],j['val'],j['offset'])
         cs.add_op(op)
     return cs
+
+
+def call_counter(fn):
+    def wrapper(*args, **kwargs):
+        wrapper.called+=1
+        return fn(*args, **kwargs)
+    wrapper.called = 0
+    wrapper.__name__=fn.__name__
+    return wrapper
