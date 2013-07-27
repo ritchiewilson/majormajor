dcdbee7979c5b5507e54b599ae6009f6b1ba9bd8
ritchiew <ritchie.a.f.wilson@gmail.com>
2013-07-27 13:30:52 -0400

    start restructuring direcotries

--- texteditor.py
+++ texteditor.py
@@ -16,8 +16,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from gi.repository import Gtk, Pango
-from majormajor import MajorMajor
-from op import Op
+from majormajor.majormajor import MajorMajor
+from majormajor.ops.op import Op
 import sys
 
 
