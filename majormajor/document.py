bd2563732b96079a8c31ab956c8cdaeae673459a
ritchiew <ritchie.a.f.wilson@gmail.com>
2013-08-17 23:20:00 -0400

    fixed code for style guides

--- majormajor/document.py
+++ majormajor/document.py
@@ -15,26 +15,25 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import json, difflib
+import difflib
 import random
-import string
 import uuid
 import copy
 from datetime import datetime
+
 from changeset import Changeset
 from ops.op import Op
 from utils import build_changeset_from_dict
+
 
 class Document:
 
     # Each document needs an ID so that changesets can be associated
     # with it. If one is not supplied, make a random 5 character ID at
     # start
-    def __init__(self, id_ = None, user=None, snapshot=None):
+    def __init__(self, id_=None, user=None, snapshot=None):
         self.id_ = id_ if id_ else uuid.uuid4()
-        if user == None:
-            user = str(uuid.uuid4())
-        self.user = user
+        self.user = user if user else str(uuid.uuid4())
         self.ordered_changesets = []
         self.ordered_changesets_set_cache = set([])
         self.all_known_changesets = {}
@@ -46,7 +45,7 @@ class Document:
         self.root_changeset = None
         self.dependencies = []
         # set initial snapshot if called upon
-        if not snapshot == None:
+        if not snapshot is None:
             self.set_initial_snapshot(snapshot)
         self.dependencies = [self.root_changeset]
         #  With an event loop, many actions happen on a timer. For
@@ -84,17 +83,17 @@ class Document:
 
     def get_send_queue(self):
         return self.send_queue[:]
-    
+
     def clear_send_queue(self):
         self.send_queue = []
-        
+
     def get_missing_dependency_ids(self, new_cs):
         missing_dep_ids = []
         for dep in new_cs.get_parents():
             if not isinstance(dep, Changeset):
                 missing_dep_ids.append(dep)
         return missing_dep_ids
-                
+
     def get_changeset_by_id(self, cs_id):
         if cs_id in self.all_known_changesets:
             return self.all_known_changesets[cs_id]['obj']
@@ -119,7 +118,7 @@ class Document:
                 cs_in_range.append(cs)
             if cs.get_id() in start_ids:
                 start_reached = True
-        return cs_in_range # TODO needed?
+        return cs_in_range  # TODO needed?
 
     def get_snapshot(self):
         return self.snapshot
@@ -128,8 +127,10 @@ class Document:
         return self.changesets
 
     def get_sync_status(self, remote_deps):
-        request_css = [dep for dep in remote_deps if not self.knows_changeset(dep)]
-        send_css = [cs for cs in self.dependencies if not cs.get_id() in remote_deps]
+        request_css = [dep for dep in remote_deps
+                       if not self.knows_changeset(dep)]
+        send_css = [cs for cs in self.dependencies
+                    if not cs.get_id() in remote_deps]
         return request_css, send_css
 
     def request_ancestors(self, cs_ids, dep_ids):
@@ -177,7 +178,6 @@ class Document:
         self.root_changeset = cs
         self.ordered_changesets = [cs]
         self.ordered_changesets_set_cache = set([cs])
-        
 
     def set_snapshot(self, snapshot, deps):
         """
@@ -191,8 +191,6 @@ class Document:
         for dep in deps:
             self.add_to_known_changesets(dep)
         self.dependencies = deps
-
-
 
     def knows_changeset(self, cs_id):
         if cs_id in self.all_known_changesets:
@@ -208,9 +206,8 @@ class Document:
         self.add_to_known_changesets(cs)
         #i = self.insert_changeset_into_changsets(cs)
         #cs.find_unaccounted_changesets(self.changesets[:i])
-        return 0 # return index of where it was stuck
+        return 0  # return index of where it was stuck
 
-        
     def add_local_op(self, op):
         """
         For when this user (not remote collaborators) add an
@@ -219,7 +216,7 @@ class Document:
         already started, the given op is just added on. The given op
         is then immediatly applied to this Document.
         """
-        if self.open_changeset == None:
+        if self.open_changeset is None:
             self.open_changeset = Changeset(self.id_, self.user,
                                             self.get_dependencies())
         self.open_changeset.add_op(op)
@@ -236,9 +233,9 @@ class Document:
         """
 
         if self.open_changeset and self.open_changeset.is_empty():
-            self.open_changeset == None
-            
-        if self.open_changeset == None:
+            self.open_changeset is None
+
+        if self.open_changeset is None:
             return False
 
         cs = self.open_changeset
@@ -262,7 +259,6 @@ class Document:
         for cs in css:
             self.receive_changeset(cs)
         return True
-        
 
     def receive_changeset(self, cs):
         """
@@ -290,13 +286,12 @@ class Document:
         was_inserted = self.pull_from_pending_list()
         return was_inserted
 
-
     def activate_changeset_in_document(self, cs):
         """
         Handles actually inserting the cs into the ordered changesets,
         resetting changesets which must do OT, performing OT, and
         reseting this document's dependency info.
-        
+
         The given cs must a be one which 1) is not already in the ordered
         changesets, 2) has all needed info to be inserted into ordered
         changesets.
@@ -306,10 +301,10 @@ class Document:
         # accuratly determined, so cache it if need be.
         if cs._is_ancestor_cache:
             cs.get_ancestors()
-        
+
         index = self.insert_changeset_into_ordered_list(cs)
         self.update_unaccounted_changesets(cs, index=index)
-        
+
         # remove document dependencies covered by this new changeset
         for parent in cs.get_parents():
             if parent in self.dependencies:
@@ -317,7 +312,6 @@ class Document:
         self.dependencies.append(cs)
         return index
 
-        
     def pull_from_pending_list(self):
         """
         Go through the list of pending changesets and try again to
@@ -328,7 +322,7 @@ class Document:
         # keep track of lowest index for start point for ot
         index = len(self.ordered_changesets)
         one_inserted = False
-        l = -1 # flag for when looping is done
+        l = -1  # flag for when looping is done
         while not l == len(self.pending_new_changesets):
             l = len(self.pending_new_changesets)
             for cs in iter(self.pending_new_changesets):
@@ -374,11 +368,11 @@ class Document:
         self.ordered_changesets_set_cache = set(self.ordered_changesets)
         for i, cs in enumerate(self.ordered_changesets):
             self.update_unaccounted_changesets(cs, i)
-            
+
     def relink_changesets(self):
         for cs in self.all_known_changesets.values():
             cs['obj'].relink_changesets(self.all_known_changesets)
-            
+
     def add_to_known_changesets(self, cs):
         """
         Add a changeset to the dict of all known changesets, then link it
@@ -440,10 +434,10 @@ class Document:
         while index > 0 and not self.ordered_changesets[index].has_valid_snapshot_cache():
             index -= 1
         if index == 0:
-            self.snapshot ={}
+            self.snapshot = {}
         else:
             self.snapshot = self.ordered_changesets[index].get_snapshot_cache()
-            index +=1
+            index += 1
         while index < len(self.ordered_changesets):
             for op in self.ordered_changesets[index].get_ops():
                 self.apply_op(op)
@@ -460,7 +454,9 @@ class Document:
         """
         unaccounted_css = []
         deps = cs.get_parents()
-        pos_of_cs = self.ordered_changesets.index(cs) if index == None else index
+        pos_of_cs = index
+        if index is None:
+            pos_of_cs = self.ordered_changesets.index(cs)
         i = pos_of_cs - 1
         while deps and i > 0:
             old_cs = self.ordered_changesets[i]
@@ -468,8 +464,8 @@ class Document:
                 if len(deps) == 1:
                     # if this is the last dep, then cs (generally)
                     # shares it's unknown dependencies
-                    rev_ucss = reversed(old_cs.get_unaccounted_changesets())
-                    ucss = [ucs for ucs in rev_ucss if not cs.has_ancestor(ucs)]
+                    rev_ucs = reversed(old_cs.get_unaccounted_changesets())
+                    ucss = [ucs for ucs in rev_ucs if not cs.has_ancestor(ucs)]
                     unaccounted_css.extend(ucss)
                     break
                 deps.remove(old_cs)
@@ -483,9 +479,10 @@ class Document:
         i = pos_of_cs + 1
         while i < len(self.ordered_changesets):
             future_cs = self.ordered_changesets[i]
-            future_cs.add_to_unaccounted_changesets(cs,pos_of_cs, self.ordered_changesets)
+            future_cs.add_to_unaccounted_changesets(cs,pos_of_cs,
+                                                    self.ordered_changesets)
             i += 1
-        
+
     def insert_changeset_into_ordered_list(self, cs):
         """
         When there is just one new changeset to add, there is no need to
@@ -498,10 +495,11 @@ class Document:
         self.ordered_changesets_set_cache.update([cs])
         return i
 
-    def get_insertion_point_into_ordered_changesets(self, cs, ordered_list=None):
-        if ordered_list == None:
+    def get_insertion_point_into_ordered_changesets(self, cs,
+                                                    ordered_list=None):
+        if ordered_list is None:
             ordered_list = self.ordered_changesets
-        
+
         # first get the most recent dependency
         deps = cs.get_parents()
         i = len(ordered_list) - 1
@@ -510,7 +508,7 @@ class Document:
 
         last_dep = ordered_list[i]
 
-        i += 1        
+        i += 1
         while i < len(ordered_list):
             if ordered_list[i].has_ancestor(cs):
                 break
@@ -520,10 +518,8 @@ class Document:
             elif not ordered_list[i].has_ancestor(last_dep):
                 break
             i += 1
-            
-        return i
 
-        
+        return i
 
     def tree_to_list(self):
         """
@@ -558,7 +554,7 @@ class Document:
 
             if loop_completed:
                 break
-                
+
             if not just_increment or len(cs.get_parents()) > 1:
                 insertion_point = self.get_insertion_point_into_ordered_changesets(cs, tree_list)
                 children = cs.get_children()
@@ -568,7 +564,7 @@ class Document:
         return tree_list
 
     def _cs_cannot_be_inserted(self, cs, tree_set_cache):
-        return (cs == None or 
+        return (cs is None or
                 set(cs.get_parents()) - tree_set_cache != set([]) or
                 cs in tree_set_cache)
 
@@ -576,11 +572,11 @@ class Document:
         """
         Accept an old snapshot and get the opcodes which turn it into the
         current snapshot.
-        TODO: this is only set up for strings right now. 
+        TODO: this is only set up for strings right now.
         """
         new_state = self.get_snapshot()
         diff = difflib.SequenceMatcher(None, old_state, new_state)
-        path = [] # just working with strings. path is always root
+        path = []  # just working with strings. path is always root
         opcodes = []
         for tag, i1, i2, j1, j2 in diff.get_opcodes():
             if tag == 'insert':
@@ -593,19 +589,16 @@ class Document:
                 opcodes.append(('replace', path, i1, (i2 - i1), txt))
 
         return opcodes
-        
 
-        
-    # To determine if the path is valid in this document
     def contains_path(self, path):
         """ Checks if the given path is valid in this document's snapshot """
         node = self.snapshot
         for i in path:
             if isinstance(i, str):
-               if not isinstance(node, dict):
-                  return False
-               if not i in node:
-                   return False
+                if not isinstance(node, dict):
+                    return False
+                if not i in node:
+                    return False
             elif isinstance(i, int):
                 if not isinstance(node, list):
                     return False
@@ -622,14 +615,14 @@ class Document:
         return node
 
     def get_value(self, path):
-        if len(path) ==0:
+        if len(path) == 0:
             return self.snapshot
         return self.get_node(path)[path[-1]]
 
     def apply_op(self, op):
         if not self.contains_path(op.path):
             return "ERROR!"
-        
+
         func_name = self.json_opperations[op.action]
         func = getattr(self, func_name)
         if len(op.path) == 0:
@@ -638,7 +631,7 @@ class Document:
             node = self.get_node(op.path)
             node[op.path[-1]] = func(op)
 
-    # JSON Opperation - wholesale replacing value at a given path        
+    # JSON Opperation - wholesale replacing value at a given path
     def set_value(self, op):
         return op.val
 
@@ -663,8 +656,7 @@ class Document:
     def string_delete(self, op):
         cur = self.get_value(op.t_path)
         return cur[:op.t_offset] + cur[op.t_offset + op.t_val:]
-        
-    
+
     def array_insert(self, op):
         cur = self.get_value(op.path)
         r = cur[:op.offset]
@@ -696,17 +688,15 @@ class Document:
         cur.pop(op.offset)
         return cur
 
-        
     json_opperations = {
         'set': 'set_value',
-        'bn' : 'boolean_negation',
-        'na' : 'number_add',
-        'si' : 'string_insert',
-        'sd' : 'string_delete',
-        'ai' : 'array_insert',
-        'ad' : 'array_delete',
-        'am' : 'array_move',
-        'oi' : 'object_insert',
-        'od' : 'object_delete'
+        'bn': 'boolean_negation',
+        'na': 'number_add',
+        'si': 'string_insert',
+        'sd': 'string_delete',
+        'ai': 'array_insert',
+        'ad': 'array_delete',
+        'am': 'array_move',
+        'oi': 'object_insert',
+        'od': 'object_delete'
     }
-
