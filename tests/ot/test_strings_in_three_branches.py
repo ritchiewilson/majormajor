844785b6cb5a4fd44649a53e1ca0ac74f6649722
ritchiew <ritchie.a.f.wilson@gmail.com>
2013-08-30 10:53:39 -0400

    Added string tests for three branches

--- tests/ot/test_strings_in_three_branches.py
+++ tests/ot/test_strings_in_three_branches.py
@@ -24,6 +24,10 @@ from majormajor.changeset import Changeset
 class TestStringsInThreeBranches:
 
     def test_three_branches(self):
+        """
+        Three branches, A B and C, delete part of the document then insert
+        their own text.
+        """
         doc = Document(snapshot='0123456789')
         doc.HAS_EVENT_LOOP = False
         root = doc.get_root_changeset()
@@ -61,3 +65,89 @@ class TestStringsInThreeBranches:
         C1.set_id('C1')
         doc.receive_changeset(C1)
         assert doc.get_snapshot() == '01289AAAAABBBBBCCCCC'
+
+    def test_three_branches_change_order_1(self):
+        """
+        Same test as above, but changing the order in which the branches are
+        applied to B A C.
+        """
+        doc = Document(snapshot='0123456789')
+        doc.HAS_EVENT_LOOP = False
+        root = doc.get_root_changeset()
+
+        A0 = Changeset(doc.get_id(), 'u1', [root])
+        A0.add_op(Op('sd', [], offset=3, val=3))
+        A0.set_id('1A0')
+        doc.receive_changeset(A0)
+
+        A1 = Changeset(doc.get_id(), 'u1', [A0])
+        A1.add_op(Op('si', [], offset=7, val='AAAAA'))
+        A1.set_id('A1')
+        doc.receive_changeset(A1)
+        assert doc.get_snapshot() == '0126789AAAAA'
+
+        B0 = Changeset(doc.get_id(), 'u1', [root])
+        B0.add_op(Op('sd', [], offset=4, val=3))
+        B0.set_id('0B0')
+        doc.receive_changeset(B0)
+
+        B1 = Changeset(doc.get_id(), 'u1', [B0])
+        B1.add_op(Op('si', [], offset=7, val='BBBBB'))
+        B1.set_id('B1')
+        doc.receive_changeset(B1)
+        assert doc.get_snapshot() == '012789BBBBBAAAAA'
+
+        C0 = Changeset(doc.get_id(), 'u1', [root])
+        C0.add_op(Op('sd', [], offset=5, val=3))
+        C0.set_id('2C0')
+        doc.receive_changeset(C0)
+        assert doc.get_snapshot() == '01289BBBBBAAAAA'
+
+        C1 = Changeset(doc.get_id(), 'u1', [C0])
+        C1.add_op(Op('si', [], offset=7, val='CCCCC'))
+        C1.set_id('C1')
+        doc.receive_changeset(C1)
+        assert doc.get_snapshot() == '01289BBBBBAAAAACCCCC'
+
+    def test_three_branches_change_order_2(self):
+        """
+        Again, same test, but changing the order in which the branches are
+        applied to C B A.
+        """
+        doc = Document(snapshot='0123456789')
+        doc.HAS_EVENT_LOOP = False
+        root = doc.get_root_changeset()
+
+        A0 = Changeset(doc.get_id(), 'u1', [root])
+        A0.add_op(Op('sd', [], offset=3, val=3))
+        A0.set_id('2A0')
+        doc.receive_changeset(A0)
+
+        A1 = Changeset(doc.get_id(), 'u1', [A0])
+        A1.add_op(Op('si', [], offset=7, val='AAAAA'))
+        A1.set_id('A1')
+        doc.receive_changeset(A1)
+        assert doc.get_snapshot() == '0126789AAAAA'
+
+        B0 = Changeset(doc.get_id(), 'u1', [root])
+        B0.add_op(Op('sd', [], offset=4, val=3))
+        B0.set_id('1B0')
+        doc.receive_changeset(B0)
+
+        B1 = Changeset(doc.get_id(), 'u1', [B0])
+        B1.add_op(Op('si', [], offset=7, val='BBBBB'))
+        B1.set_id('B1')
+        doc.receive_changeset(B1)
+        assert doc.get_snapshot() == '012789BBBBBAAAAA'
+
+        C0 = Changeset(doc.get_id(), 'u1', [root])
+        C0.add_op(Op('sd', [], offset=5, val=3))
+        C0.set_id('0C0')
+        doc.receive_changeset(C0)
+        assert doc.get_snapshot() == '01289BBBBBAAAAA'
+
+        C1 = Changeset(doc.get_id(), 'u1', [C0])
+        C1.add_op(Op('si', [], offset=7, val='CCCCC'))
+        C1.set_id('C1')
+        doc.receive_changeset(C1)
+        assert doc.get_snapshot() == '01289CCCCCBBBBBAAAAA'
